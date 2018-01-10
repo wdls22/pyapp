1c21afcb5416fa32bc11130c14407877fbe1499f
wdls <wdls@wdlsdeMacBook-Air.local>
2018-01-11 00:41:01 +0800

    first commit

--- wdls/views.py
+++ wdls/views.py
@@ -13,16 +13,16 @@ from flask import Flask, request, session, g, redirect, url_for, abort, \
 
 ISOTIMEFORMAT = '%Y-%m-%d %X'
 
-class RegistrationForm(Form):
-    username = TextField('Username', [validators.Length(min=3, max=25)])
-    email = TextField('Email Address', [validators.Length(min=6, max=35)])
-    password = PasswordField('New Password', [
-        validators.Required(),
-        validators.EqualTo('confirm', message='Passwords must match')
-    ])
-    confirm = PasswordField('Repeat Password')
-    accept_tos = BooleanField('I accept the TOS', [validators.Required()])
-
+# class RegistrationForm(Form):
+#     username = TextField('Username', [validators.Length(min=3, max=25)])
+#     email = TextField('Email Address', [validators.Length(min=6, max=35)])
+#     password = PasswordField('New Password', [
+#         validators.Required(),
+#         validators.EqualTo('confirm', message='Passwords must match')
+#     ])
+#     confirm = PasswordField('Repeat Password')
+#     accept_tos = BooleanField('I accept the TOS', [validators.Required()])
+#
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
