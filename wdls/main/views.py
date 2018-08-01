c686ecffc71ba9755409d9ffd621802accc5f7ce
Sheng Li <wdls222@gmail.com>
2018-08-01 23:17:17 +0800

    1

--- wdls/main/views.py
+++ wdls/main/views.py
@@ -239,9 +239,7 @@ def wechat():
         else:
             return ""
     if request.method == "POST":
-        xmldata = request.args
-        print xmldata
-        xml_rec = ET.fromstring(xmldata)
+        xml_rec = ET.fromstring(request.args)
         ToUserName = xml_rec.find('ToUserName').text
         fromUser = xml_rec.find('FromUserName').text
         MsgType = xml_rec.find('MsgType').text
@@ -255,3 +253,20 @@ def wechat():
                 <MsgType>![CDATA[text]]</MsgType>
                 <Content>![CDATA[%s]]</Content>
                 </xml>''' % (fromUser, ToUserName, int(time()), Content)
+
+        xml_recv = ET.fromstring(request.data)  #获取用户发送的原始数据
+        ToUserName = xml_recv.find("ToUserName").text   #获取之前发送的 目标用户（公众号）
+        FromUserName = xml_recv.find("FromUserName").text #获取之前的 消息来源用户
+        Content = xml_recv.find("Content").text     #获取之前 向服务器发送的消息
+                                                #构造xml格式，回复内容
+        reply = """<xml>
+                  <ToUserName> <![CDATA[%s]]></ToUserName>
+                  <FromUserName><![CDATA[%s]]></FromUserName>
+                  <CreateTime>%s</CreateTime>
+                  <MsgType><![CDATA[text]]></MsgType>
+                  <Content><![CDATA[%s]]></Content>
+                  </xml>"""
+
+        response = make_response(reply % (FromUserName, ToUserName, str(int(time.time())), Content))
+        response.content_type = 'application/xml'
+        return response 
